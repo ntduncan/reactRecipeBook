f2eda68e05fa750e0cc7c85c4b28c801b402004d
Yu-Chun Tung <dyj0183@byui.edu>
2022-01-19 11:36:49 -0700

    render dummy recipe data on the Card

--- recipe-book/src/App.js
+++ recipe-book/src/App.js
@@ -4,7 +4,7 @@ import Recipes from "./components/Recipes/Recipes";
 
 const recipes = [
 	{
-    id: 1,
+		id: 1,
 		title: "Spaghetti Carbonara",
 		servings: 2,
 		ingredients: [
@@ -19,9 +19,9 @@ const recipes = [
 		meal_type: ["lunch", "dinner"],
 	},
 	{
-    id: 2,
+		id: 2,
 		title: "Avacodo Toast",
-		servings: 2,
+		servings: 1,
 		ingredients: [
 			"1 lb spaghetti",
 			"3.5 oz pancetta",
@@ -29,12 +29,27 @@ const recipes = [
 			"1  egg",
 			"0.5 cup parmesan cheese",
 		],
-		instructions: "simply add avacodo on top of your toast. If you enjoy your avacado guac style, just mash it.",
+		instructions:
+			"Simply add avacodo on top of your toast. If you enjoy your avacado guac style, just mash it.",
 		meal_type: ["breakfast"],
 	},
 	{
-    id: 3,
+		id: 3,
 		title: "Apple Pie",
+		servings: 5,
+		ingredients: [
+			"1 lb spaghetti",
+			"3.5 oz pancetta",
+			"2 Tbsps olive oil",
+			"1  egg",
+			"0.5 cup parmesan cheese",
+		],
+		instructions: "Simply put apples on your pie and bake it for 3 hours.",
+		meal_type: ["dessert"],
+	},
+	{
+		id: 4,
+		title: "Spaghetti Carbonara",
 		servings: 2,
 		ingredients: [
 			"1 lb spaghetti",
@@ -43,38 +58,25 @@ const recipes = [
 			"1  egg",
 			"0.5 cup parmesan cheese",
 		],
-		instructions: "simply put apples on your pie and bake it for 3 hours.",
-		meal_type: ["dessert"],
+		instructions:
+			"Bring a large pot of water to a boil and season generously with salt. Add the pasta to the water once boiling and cook until al dente. Reserve 2 cups of cooking water and drain the pasta. ",
+		meal_type: ["lunch", "dinner"],
 	},
 	{
-	id: 4,
-	title: "Spaghetti Carbonara",
-	servings: 2,
-	ingredients: [
-		"1 lb spaghetti",
-		"3.5 oz pancetta",
-		"2 Tbsps olive oil",
-		"1  egg",
-		"0.5 cup parmesan cheese",
-	],
-	instructions:
-		"Bring a large pot of water to a boil and season generously with salt. Add the pasta to the water once boiling and cook until al dente. Reserve 2 cups of cooking water and drain the pasta. ",
-	meal_type: ["lunch", "dinner"],
-},
-{
-id: 5,
-	title: "Avacodo Toast",
-	servings: 2,
-	ingredients: [
-		"1 lb spaghetti",
-		"3.5 oz pancetta",
-		"2 Tbsps olive oil",
-		"1  egg",
-		"0.5 cup parmesan cheese",
-	],
-	instructions: "simply add avacodo on top of your toast. If you enjoy your avacado guac style, just mash it.",
-	meal_type: ["breakfast"],
-},
+		id: 5,
+		title: "Avacodo Toast",
+		servings: 1,
+		ingredients: [
+			"1 lb spaghetti",
+			"3.5 oz pancetta",
+			"2 Tbsps olive oil",
+			"1  egg",
+			"0.5 cup parmesan cheese",
+		],
+		instructions:
+			"Simply add avacodo on top of your toast. If you enjoy your avacado guac style, just mash it.",
+		meal_type: ["breakfast"],
+	},
 ];
 
 function App() {
--- recipe-book/src/components/Recipes/RecipeItem.js
+++ recipe-book/src/components/Recipes/RecipeItem.js
@@ -11,15 +11,22 @@ const RecipeItem = ({ recipe }) => {
 					<img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/169963/photo-1429043794791-eb8f26f44081.jpeg" />
 				</div>
 				<div className="post-content">
-					<div className="category">Photos</div>
-					<h1 className="title">City Lights in New York</h1>
-					<h2 className="sub_title">The city that never sleeps.</h2>
+					<div className="category">
+						{recipe.meal_type.map((type) => {
+							return type + " ";
+						})}
+					</div>
+					<h1 className="title">{recipe.title}</h1>
+					<h2 className="sub_title">Servings: {recipe.servings}</h2>
+					<h3>Instructions: </h3>
+					<p className="description">{recipe.instructions}</p>
+					<h3>Ingredients: </h3>
 					<p className="description">
-						New York, the largest city in the U.S., is an architectural marvel
-						with plenty of historic monuments, magnificent buildings and
-						countless dazzling skyscrapers.
+						{recipe.ingredients.map((ingredient) => {
+							return ingredient + ", ";
+						})}
 					</p>
-					<div className="post-meta">
+					{/* <div className="post-meta">
 						<span className="timestamp">
 							<i className="fa fa-clock-">o</i> 6 mins ago
 						</span>
@@ -27,7 +34,7 @@ const RecipeItem = ({ recipe }) => {
 							<i className="fa fa-comments"></i>
 							<a href="#"> 39 comments</a>
 						</span>
-					</div>
+					</div> */}
 				</div>
 			</div>
 		</div>
